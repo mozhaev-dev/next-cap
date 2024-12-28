95f298a851268e60ebeab7265d39f932266b9960
Sergei.Mozhaev <mozhaev.dev@gmail.com>
2024-12-28 09:49:58 +0200

    init

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import deletePlugin from 'rollup-plugin-delete'
+const isProduction = process.env.NODE_ENV === 'production'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: !isProduction,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    deletePlugin({ targets: 'dist/*' }),
+    isProduction && terser(),
+  ],
+  external: [],
+}
